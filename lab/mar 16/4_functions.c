8616204f5075fa8dcd8ba00b4bacf6d43ba1a4cd
Guru R <gururaja.spr@gmail.com>
2023-03-16 21:23:12 +0530

    mar 16

---
+++ lab/mar 16/4_functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void factorial(int n)
+{
+    int fact = 1;
+    for (i = 1; i <= n; i++)
+        fact = fact * i;
+    printf("factorial of %d is %d", n, fact);
+}
+void qr(int n1, n2)
+{
+    q = n1 / n2;
+    r = n1 % n2;
+    printf("quotient is %d\nremainder is %d", q, r);
+}
+void roots(a, b, c)
+{
+    int x, y;
+    x = -b + sqrt(b*b)
+}
+
+int main()
+{
+    
+
+    return 0;
+}
