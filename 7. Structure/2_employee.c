e065175ce6e2d3e0909f978314dd94f4b31ca35d
Guru-25 <guru@duck.com>
2023-05-10 23:32:52 +0530

    add employee structure problem

---
+++ 7. Structure/2_employee.c
@@ -0,0 +1,39 @@
+// Employee payroll details and its operations
+
+#include <stdio.h>
+#include <string.h>
+
+struct employee {
+    char name[20];
+    float bp, da, hra, it, gsal, net;
+};
+
+void main() {
+    int n = 2, i;
+    struct employee emp_no[n];
+    for (i = 0; i < n; i++) {
+        printf("Enter Name of Employee %d: ", i+1);
+        scanf("%s", emp_no[i].name);
+        printf("Enter Salary details (BP DA HRA) of Employee %d: ", i+1);
+        scanf("%f %f %f", &emp_no[i].bp, &emp_no[i].da, &emp_no[i].hra);
+        printf("Enter Income Tax of Employee %d: ", i+1);
+        scanf("%f", &emp_no[i].it);
+        emp_no[i].gsal = emp_no[i].bp + emp_no[i].da + emp_no[i].hra;
+        emp_no[i].net = emp_no[i].gsal - emp_no[i].it;
+        printf("\n");
+    }
+    float highest = emp_no[0].net;
+    char highName[20];
+    for (i = 0; i < n; i++) {
+        if (emp_no[i].net > highest) {
+            highest = emp_no[i].net;
+            strcpy(highName, emp_no[i].name);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        printf("Name of Employee %d: %s\n", i + 1, emp_no[i].name);
+        printf("Gross Salary Employee of %d: %.2f\n", i + 1, emp_no[i].gsal);
+        printf("Net Salary of Employee %d: %.2f\n\n", i + 1, emp_no[i].net);
+    }
+    printf("Employee %s is having the highest salary of %.2f", highName, highest);
+}
